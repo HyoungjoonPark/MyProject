15f08aaa9f6f7541e23b7b22f1dd7331dd10ea0d
HyoungJoonPark <egreen777@gmail.com>
2016-10-30 20:37:34 +0900

    package changed

---
+++ Helloworld/src/com/egreen7/androidstudy/Helloworld.java
@@ -0,0 +1,10 @@
+package com.egreen7.androidstudy; 
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello my world");
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		System.out.println(2*5);
+	}
+}
